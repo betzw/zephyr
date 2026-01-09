e965366d96f92a59f77b61f3d0a851f98bb79a50
Wolfgang Betz <wolfgang.betz@st.com>
2026-01-09 15:04:27 +0100

    STM32N6: allow for enabling/disabling the NPU cache via `CONFIG_STM32N6_ENABLE_NPU_CACHE`
    Note: default value is  `y`.

--- soc/st/stm32/stm32n6x/npu/npu_stm32n6.c
+++ soc/st/stm32/stm32n6x/npu/npu_stm32n6.c
@@ -8,12 +8,35 @@
 
 #include <errno.h>
 
+#include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/reset.h>
 #include <zephyr/init.h>
 #include <soc.h>
 
 #include <zephyr/drivers/clock_control/stm32_clock_control.h>
+
+#ifdef CONFIG_STM32N6_ENABLE_NPU_CACHE
+static void _npu_cache_axi_enable(void)
+{
+	/* Disable cache */
+	*((__IO uint32_t *)(CACHEAXI_BASE_S)) = 0x0;
+ 
+	k_busy_wait(5 * 1000); // 5ms delay
+
+	/* Enable cache */
+	*((__IO uint32_t *)(CACHEAXI_BASE_S)) = 0x1;
+
+	/* Enable cache counters */
+	*((__IO uint32_t *)(CACHEAXI_BASE_S)) |= 0x33330000;
+
+	/* Reset cache counters */
+	*((__IO uint32_t *)(CACHEAXI_BASE_S)) |= 0xcccc0000;
+
+	/* Enable cache error interrupt */
+	*((__IO uint32_t *)(CACHEAXI_BASE_S + 8)) = (1 << 2);
+}
+#endif /* CONFIG_STM32N6_ENABLE_NPU_CACHE */
 
 /* Read-only driver configuration */
 struct npu_stm32_cfg {
@@ -49,6 +72,10 @@ static int npu_stm32_init(const struct device *dev)
 	/* Reset timer to default state using RCC */
 	(void)reset_line_toggle_dt(&cfg->reset_npu);
 	(void)reset_line_toggle_dt(&cfg->reset_cacheaxi);
+
+#ifdef CONFIG_STM32N6_ENABLE_NPU_CACHE
+	_npu_cache_axi_enable();
+#endif /* CONFIG_STM32N6_ENABLE_NPU_CACHE */
 
 	return 0;
 }
